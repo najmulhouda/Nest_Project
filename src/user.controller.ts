2ff6b4a607d93dd3960cb117938a82b015dd94d4
najmulhouda <najmulhouda6@gmail.com>
2024-03-06 21:12:34 +0600

    Http Httpcode Httpstatus

--- src/user.controller.ts
+++ src/user.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, HttpCode, HttpStatus, Post, Req } from '@nestjs/common';
 import { Request } from 'express';
 import { of } from 'rxjs';
 
 @Controller('users')
 export class UsersConttroller {
-  @Get('/profile')
+  @Post('/profile')
+  @HttpCode(HttpStatus.RESET_CONTENT)
   getProfile(@Req() req: Request) {
     console.log(req);
     return of({

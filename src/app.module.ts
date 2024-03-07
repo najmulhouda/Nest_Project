998f1750ae75f3f9016c020ddcb63d7a40ebedb5
najmulhouda <najmulhouda6@gmail.com>
2024-03-07 19:44:54 +0600

    dependency injection, standtard providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
-
+import { UsersStore } from './users.store';
 @Module({
   controllers: [UsersController],
+  providers: [UsersStore],
 })
 export class AppModule {}

7f87d0c5da915229e6cc00c806cbf83a9d46dec9
najmulhouda <najmulhouda6@gmail.com>
2024-03-07 13:12:04 +0600

    crud op with nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './user.controller';
+import { UsersController } from './users.controller';
 
 @Module({
   controllers: [UsersController],

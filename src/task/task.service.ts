4bf30111941c15a991ea16ec93603061c04aca20
najmulhouda <najmulhouda6@gmail.com>
2024-03-26 23:59:21 +0600

    task service updated

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from './task.model';
-
+import { Task, TaskStatus } from './task.model';
 @Injectable()
 export class TaskService {
   private tasks: Task[] = [];
   getAllTask(): Task[] {
     return this.tasks;
+  }
+
+  createTask(title: string, description: string): Task {
+    const task: Task = {
+      id: uuidv4(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
   }
 }

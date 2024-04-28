7ef8a3f62b0fcfe827c8b531650fd609dd3dd973
najmulhouda <najmulhouda6@gmail.com>
2024-04-28 23:30:47 +0600

    typeoorm setup

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,10 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeormConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'datab',
+};
--- src/task/pipes/task-status-validation.pipe.ts
+++ src/task/pipes/task-status-validation.pipe.ts
@@ -21,5 +21,3 @@ export class TaskStatusValidationPipe implements PipeTransform {
     return idx !== -1;
   }
 }
-//database connection problem
-//pgadmin not connected

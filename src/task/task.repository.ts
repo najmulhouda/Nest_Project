50264fb3e479105f9f1ac16885e42c55a4ec6a3a
najmulhouda <najmulhouda6@gmail.com>
2024-05-06 13:29:20 +0600

    task repository file created

---
+++ src/task/task.repository.ts
@@ -0,0 +1,10 @@
+import { Repository } from 'typeorm';
+import { Task } from './task.entity';
+
+export class TaskRepository extends Repository<Task> {
+  // get all tasks
+  // add task
+  // get task
+  // delete task
+  // update task
+}

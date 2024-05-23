bc17e3de45220da997fd809891f7da1e774173d5
najmulhouda <najmulhouda6@gmail.com>
2024-05-23 14:34:05 +0600

    creating task 2

--- src/task/task.repository.ts
+++ src/task/task.repository.ts
@@ -1,5 +1,28 @@
 import { EntityRepository, Repository } from 'typeorm';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { TaskStatus } from './task-status.enum';
 import { Task } from './task.entity';
 
 @EntityRepository(Task)
-export class TaskRepository extends Repository<Task> {}
+export class TaskRepository extends Repository<Task> {
+  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    const { title, description } = createTaskDto;
+    const task = new Task();
+    task.title = title;
+    task.description = description;
+    task.status = TaskStatus.OPEN;
+    await task.save();
+    return task;
+  }
+}
+
+// import { Repository } from 'typeorm';
+// import { Task } from './task.entity';
+
+// export class TaskRepository extends Repository<Task> {
+//   // get all tasks
+//   // add task
+//   // get task
+//   // delete task
+//   // update task
+// }

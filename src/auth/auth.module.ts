05056c117546c7e6ff92ffcdff1b29f32b424775
najmulhouda <najmulhouda6@gmail.com>
2024-07-02 21:43:20 +0600

    setting up the JWT Module and assport.js

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,21 @@
 //auth module
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { UserRepository } from './user.repository';
+import { User } from './user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'secret',
+      signOptions: { expiresIn: 3600 },
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
